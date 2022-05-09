6c28a7dd6e6a9b26403c580a2cfd057b2782c3e4
gdclone <105204508+gdclone@users.noreply.github.com>
2022-05-10 02:22:39 +0600

    Update __init__.py

--- GPSiteInfoBot/__init__.py
+++ GPSiteInfoBot/__init__.py
@@ -25,7 +25,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
         "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
     quit(1)
 
-ENV = bool(os.environ.get("ENV", ANYTHING))
+ENV = bool(os.environ.get("ENV", true))
 
 if ENV:
     TOKEN = os.environ.get("TOKEN", None)
