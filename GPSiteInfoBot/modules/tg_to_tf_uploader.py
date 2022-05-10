9a16eb226ea91ffbb5da08261e0fd1705e4ef818
gdclone <105204508+gdclone@users.noreply.github.com>
2022-05-10 23:04:19 +0600

    Create tg_to_tf_uploader.py

---
+++ GPSiteInfoBot/modules/tg_to_tf_uploader.py
@@ -0,0 +1,152 @@
+import aiohttp
+import asyncio
+import datetime
+import os
+import math
+import time
+import traceback
+
+from telethon import events
+
+from GPSiteInfoBot import telethn as bot
+
+
+def convert_from_bytes(size):
+    power = 2 ** 10
+    n = 0
+    units = {0: "", 1: "kilobytes", 2: "megabytes", 3: "gigabytes", 4: "terabytes"}
+    while size > power:
+        size /= power
+        n += 1
+    return f"{round(size, 2)} {units[n]}"
+
+
+async def download_file(url, file_name, message, start_time, bot):
+    async with aiohttp.ClientSession() as session:
+        time.time()
+        await download_coroutine(session, url, file_name, message, start_time, bot)
+    return file_name
+
+
+def get_date_in_two_weeks():
+    """
+    get maximum date of storage for file
+    :return: date in two weeks
+    """
+    today = datetime.datetime.today()
+    date_in_two_weeks = today + datetime.timedelta(days=14)
+    return date_in_two_weeks.date()
+
+
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+async def progress(current, total, event, start, type_of_ps):
+    """Generic progress_callback for both
+    upload.py and download.py"""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "[{0}{1}]\nPercent: {2}%\n".format(
+            "".join(["✅" for i in range(math.floor(percentage / 10))]),
+            "".join(["☑" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time))
+        await event.edit("{}\n {}".format(type_of_ps, tmp))
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+        + ((str(milliseconds) + "ms, ") if milliseconds else ""))
+    return tmp[:-2]
+
+
+async def send_to_transfersh_async(file):
+
+    size = os.path.getsize(file)
+    size_of_file = humanbytes(size)
+    final_date = get_date_in_two_weeks()
+    file_name = os.path.basename(file)
+
+    print("\nUploading file: {} (size of the file: {})".format(file_name, size_of_file))
+    url = "https://transfer.sh/"
+
+    with open(file, "rb") as f:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, data={str(file): f}) as response:
+                download_link = await response.text()
+
+    print(
+        "Link to download file (will be saved till {}):\n{}".format(
+            final_date, download_link
+        )
+    )
+    return download_link, final_date, size_of_file
+
+
+@bot.on(events.NewMessage(pattern="/tf"))
+async def tsh(event):
+    if event.reply_to_msg_id:
+        start = time.time()
+        url = await event.get_reply_message()
+        ilk = await event.respond("Downloading...")
+        try:
+            file_path = await url.download_media(
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, ilk, start, "Downloading...")
+                )
+            )
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            await event.respond(f"Downloading Failed\n\n**Error:** {e}")
+
+        await ilk.delete()
+
+        try:
+            orta = await event.respond("Uploading to TransferSh...")
+            download_link, final_date, size = await send_to_transfersh_async(file_path)
+
+            str(time.time() - start)
+            await orta.edit(
+                f"File Successfully Uploaded to TransferSh.\n\nLink : {download_link}\n\nExpired Date : {final_date}\n\nUploaded by @Optimus_Prime_Pro_Bot"
+            )
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            await event.respond(f"Uploading Failed\n\n**Error:** {e}")
+
+    raise events.StopPropagation
+
+
+if __name__ == "__main__":
+    main()
