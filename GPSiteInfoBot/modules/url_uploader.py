8dd1065846fd20d71f7b7faaa28973758b57e9fc
gdclone <105204508+gdclone@users.noreply.github.com>
2022-05-12 08:57:01 +0600

    Update url_uploader.py

--- GPSiteInfoBot/modules/url_uploader.py
+++ GPSiteInfoBot/modules/url_uploader.py
@@ -194,7 +194,7 @@ async def up(event):
                 event.chat.id,
                 dosya,
                 force_document=True,
-                caption=f"Uploaded By @G_Drive_Clone_Bot)
+                caption=f"Uploaded By @G_Drive_Clone_Bot")
         except Exception as e:
             traceback.print_exc()
 
